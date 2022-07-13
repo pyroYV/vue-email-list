c8afdf29040ec7ac153563a3e807f15ff4935de8
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-13 14:12:39 +0200

    ❌observer issue

--- main.js
+++ main.js
@@ -5,7 +5,23 @@ const app = new Vue({
 
     },
     methods: {
-        
+        pushRandomMail(){
+           axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+           .then(function(response){
+            
+            const result = response.response
+            this.secretArray.push(result)
+           })
+        },
+        countToTen(){
+            for (let index = 0; index < 9; index++) {
+                this.pushRandomMail()
+                console.log(this.secretArray)
+            }
+        },
+    },
+    mounted: function() {
+        this.countToTen()
     },
 
 })
